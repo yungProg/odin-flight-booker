96b9bebc7cbb3ea0a7a3cbcbc0c04f6416f7b617
yungProg <22945393+yungProg@users.noreply.github.com>
2025-12-24 02:58:09 +0000

    create flights controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "flights#index"
+
+  resources :flights
 end

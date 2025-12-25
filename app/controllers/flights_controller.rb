f0aa7c78ec15f7a2d5c014f2d3641e471ab93a18
yungProg <22945393+yungProg@users.noreply.github.com>
2025-12-25 00:37:03 +0000

    flight index form has airport codes as values instead of airport id

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,7 @@
 class FlightsController < ApplicationController
   def index
-    @departure_airports = Flight.all.map { |flight| [ flight.departure_airport.airport_code, flight.id ] }
-    @arrival_airports = Flight.all.map { |flight| [ flight.arrival_airport.airport_code, flight.id ] }
+    @departure_airports = Flight.all.map { |flight| [ flight.departure_airport.airport_code, flight.departure_airport.airport_code ] }
+    @arrival_airports = Flight.all.map { |flight| [ flight.arrival_airport.airport_code, flight.arrival_airport.airport_code ] }
     @number_of_passengers = [ [ 1, 1 ], [ 2, 2 ], [ 3, 3 ], [ 4, 4 ] ]
     @departure_dates = Flight.all.map { |flight| [ flight.start_datetime.to_date, flight.start_datetime ] }
   end

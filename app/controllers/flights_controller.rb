ae8a8aa700ccc56b5b8a617da689e2826076a26f
yungProg <22945393+yungProg@users.noreply.github.com>
2025-12-26 01:28:45 +0000

    search for flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,8 +1,10 @@
 class FlightsController < ApplicationController
   def index
-    @departure_airports = Flight.all.map { |flight| [ flight.departure_airport.airport_code, flight.departure_airport.airport_code ] }
-    @arrival_airports = Flight.all.map { |flight| [ flight.arrival_airport.airport_code, flight.arrival_airport.airport_code ] }
+    @departure_airports = Flight.all.map { |flight| [ flight.departure_airport.airport_code, flight.departure_airport.id ] }
+    @arrival_airports = Flight.all.map { |flight| [ flight.arrival_airport.airport_code, flight.arrival_airport.id ] }
     @number_of_passengers = [ [ 1, 1 ], [ 2, 2 ], [ 3, 3 ], [ 4, 4 ] ]
     @departure_dates = Flight.all.map { |flight| [ flight.start_datetime.to_date, flight.start_datetime ] }
+
+    @queried_flights = Flight.where("departure_airport_id = :dai AND arrival_airport_id = :aai AND number_of_passengers = :nop AND start_datetime >= :sd", dai: params[:departure_airport], aai: params[:arrival_airport], nop: params[:number_of_passengers], sd: params[:start_datetime])
   end
 end
